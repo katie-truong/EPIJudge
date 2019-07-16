809984b80b39135576aa6cb85e3f5766b1ff2fe7
Katie Truong <katietruong.ucdavis@gmail.com>
2019-07-16 15:18:31 -0700

    Best time to sell stocks

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices):
-    # TODO - you fill in here.
-    return 0.0
+    min_price = float('inf')
+    max_profit = 0
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+    return max_profit
 
 
 if __name__ == '__main__':
