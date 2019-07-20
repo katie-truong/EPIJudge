482f3ec46ee578397899cfc9a1cc79c2e1f747df
Katie Truong <katietruong.ucdavis@gmail.com>
2019-07-20 00:06:52 -0700

    even_odd

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -1,9 +1,36 @@
 from test_framework import generic_test
 
+"""
+[12,11,13,9,12,8,14,13,15]
+
+[ 0, 0, 2,2,3,]
+
+[9,12,8,15]
+"""
+
 
 def buy_and_sell_stock_twice(prices):
-    # TODO - you fill in here.
-    return 0.0
+    first_profits = []
+
+    # Forward phase:
+    max_profit = 0
+    min_price = float('inf')
+    for i in range(prices):
+        min_price = min(min_price, prices[i])
+        max_profit = max(max_profit, prices[i] - min_price)
+        first_profits.append(max_profit)
+
+    # Backward phase:
+    max_price = float('-inf')
+    for i in range(len(prices)-1, -1, -1):
+        max_price = max(max_price, prices[i])
+        max_profit = max(max_profit, max_price + first_profits[i-1])
+
+    return max_profit
+
+
+    
+
 
 
 if __name__ == '__main__':
--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -5,10 +5,21 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+"""
+Your input is an array of integers, and you have to reorder its entries so that the even entries appear first. 
+This is easy if you use O(n) space, where n is the length of the array. 
+However, you are required to solve it without allocating additional storage.
+"""
 
 def even_odd(A):
-    # TODO - you fill in here.
-    return
+    next_even = 0
+    next_odd = len(A) - 1
+    while next_even < next_odd:
+        if A[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -= 1
 
 
 @enable_executor_hook
